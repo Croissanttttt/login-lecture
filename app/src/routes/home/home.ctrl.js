b065d45d67899aa23f2ddcc7b710dea6c5cfadd0
Croissanttttt <joyson1125@hanmail.net>
2024-05-12 12:39:37 +0900

    login feature

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict"
 
-const hello = (req, res) => {
-    res.render("home/index");
+const users = {
+    id: ["dev", "test", "admin"],
+    password: ["asdf", "1234", "admin"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-}
+const output = {
+    hello: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            password = req.body.password;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.password[idx] === password) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "login failed",
+        });
+    },
+};
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 };
